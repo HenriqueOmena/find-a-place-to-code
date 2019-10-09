8a84f2a3951096dc2b659e9fe307e3a9020126ba
Henrique Omena <88lmao@gmail.com>
2019-10-09 23:07:50 +0100

    divide routes

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const routes = express.Router();
+
+
+routes.post('/users', (request, response) => {
+    return response.json(request.body);
+})
+
+module.exports = routes;
